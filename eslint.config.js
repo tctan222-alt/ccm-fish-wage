ae85aa9083d0123aeb31905784a174b3d362c823
tctan222-alt <tctan222@gmail.com>
2026-07-28 20:45:11 +0800

    Fix ESLint flat config syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,31 @@
-import js from '@eslint/js'; import globals from 'globals'; import tseslint from 'typescript-eslint'; import hooks from 'eslint-plugin-react-hooks'; import refresh from 'eslint-plugin-react-refresh';
-export default tseslint.config({ignores:['dist']},js.configs.recommended,...tseslint.configs.recommended,{files:['**/*.{ts,tsx}'],languageOptions:{globals:{...globals.browser,...globals.node}},plugins:{'react-hooks':hooks,'react-refresh':refresh},rules:{...hooks.configs.recommended.rules,'react-refresh/only-export-components':['warn',{allowConstantExport:true}]}}})
+import js from '@eslint/js'
+import globals from 'globals'
+import hooks from 'eslint-plugin-react-hooks'
+import refresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      'react-hooks': hooks,
+      'react-refresh': refresh,
+    },
+    rules: {
+      ...hooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
